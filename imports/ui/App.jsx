f2afc1f1d2dbb3cb9d3012487ee07afac2d5a98e
vedransehovic <vedran.sehovic@gmail.com>
2021-04-25 13:47:24 -0400

    changes to App

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,14 @@ import Task from "./components/Task";
 import TaskForm from "./components/TaskForm";
 import { TasksCollection } from "../api/TaskCollection";
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
 export const App = () => {
   //fetch tasks from DB
   const tasks = useTracker(() =>
@@ -15,10 +23,8 @@ export const App = () => {
       <h1>Welcome to Meteor!</h1>
       <TaskForm />
       <ul>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task} />
-        ))}
-      </ul>
+      { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} />) }
+  </ul>
     </div>
   );
 };
