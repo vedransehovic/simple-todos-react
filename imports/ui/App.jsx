9c49b630aabf2fa5458fcb3cf77ecd5c17137bba
vedransehovic <vedran.sehovic@gmail.com>
2021-04-25 23:21:38 -0400

    changes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -21,19 +21,28 @@ export const App = () => {
   );
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-      <TaskForm />
-      <ul>
-        {tasks.map((task) => (
-          <Task
-            key={task._id}
-            task={task}
-            onCheckboxClick={toggleChecked}
-            onDeleteClick={deleteTask}
-          />
-        ))}
-      </ul>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>To Do List</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
